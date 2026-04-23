c18e31bb148316bb3b6daaf14f71f66a1b309f48
Ahmed Ammar <ahmed@agilekode.com>
2026-04-23 17:00:30 +0500

    chore: update STATIC_ROOT to absolute production path

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/freebase/www/freebase/staticfiles'
 
 # Media (downloaded logos)
 MEDIA_URL = '/media/'
